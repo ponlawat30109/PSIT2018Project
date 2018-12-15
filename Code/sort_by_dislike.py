839378c317475fc1b865a3321845b5ebad43321e
ponlawat30109 <ponlawat30109@users.noreply.github.com>
2018-12-15 20:30:55 +0700

    Update sort_by_dislike.py

--- Code/sort_by_dislike.py
+++ Code/sort_by_dislike.py
@@ -1,6 +1,6 @@
 import pandas,numpy,pygal
 def main():
-    df = pandas.read_csv('BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
+    df = pandas.read_csv('Data/BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
     
     vid_name = numpy.array(df['title']).tolist()
     dislikecount = numpy.array(df['dislikeCount']).tolist()
@@ -10,5 +10,5 @@ def main():
     for i in range(len(dislikecount)):
         chart.add(vid_name[i], [{'value': dislikecount[i], 'label':'{:.2f}%'.format((dislikecount[i]*100)/sum(dislikecount))}])
     chart.legend_at_bottom = True
-    chart.render_to_file('Sort By Dislike.svg')
+    chart.render_to_file('Data/Pic/Sort By Dislike.svg')
 main()
