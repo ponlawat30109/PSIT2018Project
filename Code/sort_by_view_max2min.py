44099e6f7f9fc2668f1525761919243fd19eceb0
ponlawat30109 <ponlawat30109@users.noreply.github.com>
2018-12-15 21:07:57 +0700

    Add files via upload

---
+++ Code/sort_by_view_max2min.py
@@ -0,0 +1,15 @@
+import pandas,numpy,pygal
+def main():
+    df = pandas.read_csv('BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
+    df = df.sort_values('viewCount',ascending=False)
+    
+    vid_name = numpy.array(df['title']).tolist()
+    viewcount = numpy.array(df['viewCount']).tolist()
+    
+    chart = pygal.Bar()
+    chart.title = 'Sort By View Count'
+    for i in range(10):
+        chart.add(vid_name[i], [{'value': viewcount[i], 'label':'{:.2f}%'.format((viewcount[i]*100)/sum(viewcount))}])
+    chart.legend_at_bottom = True
+    chart.render_to_file('Sort by view count max to min.svg')
+main()
