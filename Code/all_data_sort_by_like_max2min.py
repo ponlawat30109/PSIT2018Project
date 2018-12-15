0e17b1fdbfcdcd9a018f2515158495d15ce8f541
ponlawat30109 <ponlawat30109@users.noreply.github.com>
2018-12-15 21:30:32 +0700

    Add files via upload

---
+++ Code/all_data_sort_by_like_max2min.py
@@ -0,0 +1,15 @@
+import pandas,numpy,pygal
+def main():
+    df = pandas.read_csv('BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
+    df = df.sort_values('likeCount',ascending=False)
+    
+    vid_name = numpy.array(df['title']).tolist()
+    likecount = numpy.array(df['likeCount']).tolist()
+    
+    chart = pygal.Bar()
+    chart.title = 'Sort By Like maximum to minimum'
+    for i in range(len(likecount)):
+        chart.add(vid_name[i], [{'value': likecount[i], 'label':'{:.2f}%'.format((likecount[i]*100)/sum(likecount))}])
+    chart.legend_at_bottom = True
+    chart.render_to_file('All Data Sort By Llike Max to Min.svg')
+main()
---
+++ Code/all_data_sort_by_view_max2min.py
@@ -0,0 +1,15 @@
+import pandas,numpy,pygal
+def main():
+    df = pandas.read_csv('BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
+    df = df.sort_values('viewCount',ascending=False)
+    
+    vid_name = numpy.array(df['title']).tolist()
+    viewcount = numpy.array(df['viewCount']).tolist()
+    
+    chart = pygal.Bar()
+    chart.title = 'Sort By View Count maximum to minimum'
+    for i in range(len(viewcount)):
+        chart.add(vid_name[i], [{'value': viewcount[i], 'label':'{:.2f}%'.format((viewcount[i]*100)/sum(viewcount))}])
+    chart.legend_at_bottom = True
+    chart.render_to_file('All Data Sort By View Count Max to Min.svg')
+main()
