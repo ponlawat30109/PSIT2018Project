baae004b0b059a2df3dd3bb20f4598897b00ba4d
ponlawat30109 <ponlawat30109@users.noreply.github.com>
2018-12-15 21:36:02 +0700

    Update top10_sort_by_view_max2min.py

--- Code/top10_sort_by_view_max2min.py
+++ Code/top10_sort_by_view_max2min.py
@@ -1,6 +1,6 @@
 import pandas,numpy,pygal
 def main():
-    df = pandas.read_csv('BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
+    df = pandas.read_csv('Data/BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
     df = df.sort_values('viewCount',ascending=False)
     
     vid_name = numpy.array(df['title']).tolist()
@@ -11,5 +11,5 @@ def main():
     for i in range(10):
         chart.add(vid_name[i], [{'value': viewcount[i], 'label':'{:.2f}%'.format((viewcount[i]*100)/sum(viewcount))}])
     chart.legend_at_bottom = True
-    chart.render_to_file('Top 10 Sort by view count max to min.svg')
+    chart.render_to_file('Data/Pic/Top 10 Sort by view count max to min.svg')
 main()
