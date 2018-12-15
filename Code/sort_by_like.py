4b0e497402c5b881b1ce87af7010d236c1f7c4cd
ponlawat30109 <ponlawat30109@users.noreply.github.com>
2018-12-15 20:31:32 +0700

    Update sort_by_like.py

--- Code/sort_by_like.py
+++ Code/sort_by_like.py
@@ -1,6 +1,6 @@
 import pandas,numpy,pygal
 def main():
-    df = pandas.read_csv('BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
+    df = pandas.read_csv('Data/BNK48_stat_15.12.2018_14.38.csv',encoding = "UTF-8",index_col=0)
     
     vid_name = numpy.array(df['title']).tolist()
     likecount = numpy.array(df['likeCount']).tolist()
@@ -10,5 +10,5 @@ def main():
     for i in range(len(likecount)):
         chart.add(vid_name[i], [{'value': likecount[i], 'label':'{:.2f}%'.format((likecount[i]*100)/sum(likecount))}])
     chart.legend_at_bottom = True
-    chart.render_to_file('Sort By Like.svg')
+    chart.render_to_file('Data/Pic/Sort By Like.svg')
 main()
